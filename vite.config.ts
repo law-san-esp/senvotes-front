1b74b4af086857540ed32d2471de72aa81b60e29
law <lawdanazoumi@gmail.com>
2024-07-23 16:24:09 +0000

    changed default port

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-require('dotenv').config({path:'./.env'});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: process.env.PORT || 3001,
-  }
 })
